e0d6b5ecb0ecda669382faa632db10880530cafa
Aidan Feldman <aidan.feldman@gsa.gov>
2020-07-27 11:07:28 -0400

    format python file using black

--- enterprise-architecture/json_to_csv.py
+++ enterprise-architecture/json_to_csv.py
@@ -2,21 +2,29 @@ import csv
 import json
 import sys
 
-
 raw = sys.stdin.read()
 data = json.loads(raw)
-data.sort(key=lambda k: k['Name'])
+data.sort(key=lambda k: k["Name"])
 
 writer = csv.writer(sys.stdout)
-fieldnames = ["Standard Name", "Description", "Category", "Status", "Deployment Type", "Approval Expiration Date"]
+fieldnames = [
+    "Standard Name",
+    "Description",
+    "Category",
+    "Status",
+    "Deployment Type",
+    "Approval Expiration Date",
+]
 writer.writerow(fieldnames)
 
 for entry in data:
-    writer.writerow([
-        entry['Name'],
-        entry['Description'],
-        entry['Category'],
-        entry['Status'],
-        entry['DeploymentType'],
-        entry['ApprovalExpirationDate'] or '-'
-    ])
+    writer.writerow(
+        [
+            entry["Name"],
+            entry["Description"],
+            entry["Category"],
+            entry["Status"],
+            entry["DeploymentType"],
+            entry["ApprovalExpirationDate"] or "-",
+        ]
+    )
